86204d9957fc87d0d4e911a144ca0a94afb6ec51
Nam NH <namptit307@gmail.com>
2015-06-08 21:48:20 +0700

    Nop bai tap buoi 3

---
+++ NguyenHoaiNam/buoi3_bai2.py
@@ -0,0 +1,5 @@
+# Bai 2:
+print "Gia bia cua mot cuon sach la: 24.95"
+print "Moi cuon duoc giam 40% va cong them phi van chuyen"
+T = 24.95*60*0.6 + 3 + 59*0.75
+print "So tien nha xuat ban phai tra la: %0.3f" % T
