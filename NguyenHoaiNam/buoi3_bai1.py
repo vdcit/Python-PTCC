ca20b337a5cb371d506c600160c9da8ffac47c2b
Nam NH <namptit307@gmail.com>
2015-06-09 10:27:27 +0700

    Chinh sua lai bai 1

--- NguyenHoaiNam/buoi3_bai1.py
+++ NguyenHoaiNam/buoi3_bai1.py
@@ -1,5 +1,5 @@
 # Bai 1
 pi = 3.14 # Gan pi = 3.14
 r = float(raw_input("Nhap ban kinh hinh cau:")) # Doc du lieu tu dau vao
-V = (4/3)*pi*(r**3)
+V = (4.0/3)*pi*(r**3)
 print "The tich hinh cau la: %0.3f" % V
