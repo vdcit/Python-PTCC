bae70b5adaba0aa3ab366d4de7966e69bea12139
Nam NH <namptit307@gmail.com>
2015-06-09 10:36:54 +0700

    Chinh sua lai bai tap

--- NguyenHoaiNam/buoi3_bai1.py
+++ NguyenHoaiNam/buoi3_bai1.py
@@ -1,5 +1,7 @@
 # Bai 1
 pi = 3.14 # Gan pi = 3.14
-r = float(raw_input("Nhap ban kinh hinh cau:")) # Doc du lieu tu dau vao
-V = (4.0/3)*pi*(r**3)
+# Gan ban kinh bang 5
+r = 5 
+# Cong thuc tinh the tich hinh cau
+V = (4.0/3) * pi * (r**3)
 print "The tich hinh cau la: %0.3f" % V
--- NguyenHoaiNam/buoi3_bai2.py
+++ NguyenHoaiNam/buoi3_bai2.py
@@ -1,5 +1,6 @@
 # Bai 2:
 print "Gia bia cua mot cuon sach la: 24.95"
 print "Moi cuon duoc giam 40% va cong them phi van chuyen"
+# Cong thuc so tien
 T = 24.95*60*0.6 + 3 + 59*0.75
 print "So tien nha xuat ban phai tra la: %0.3f" % T
