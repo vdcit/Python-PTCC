f8fc01e3e0b7781f3c58fc4cff0cda01153fc222
Minhnx <minh7992@gmail.com>
2015-06-10 08:34:27 +0700

    comment

--- NguyenXuanMinh/Bai-3.1.py
+++ NguyenXuanMinh/Bai-3.1.py
@@ -1,4 +1,6 @@
+# Ban kinh hinh cau
 r = 5
+# The tich hinh cau
 v = (4/3.0)*3.14*(r**3)
 print ('The tich cua hinh cau: %0.2f' % v)
 
--- NguyenXuanMinh/Bai-3.2.py
+++ NguyenXuanMinh/Bai-3.2.py
@@ -1,3 +1,5 @@
+# Gia tien moi cuon sach
 cost = 24.95*(60.0/100)
+# Tong gia tien
 costs = (cost + 3) + (cost + 0.75)*59 
 print costs 
