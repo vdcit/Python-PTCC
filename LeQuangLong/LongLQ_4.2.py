843541add0ce1d004fdb7d9165e6b6920bdde008
Quang Long <longsube@gmail.com>
2015-06-15 16:30:07 +0700

    Bai tap 4

---
+++ LeQuangLong/LongLQ_4.2.py
@@ -0,0 +1,12 @@
+def ve_hinh():
+	for i in range(0,3):
+		print "+ - - - + - - - +"
+		print "|       |       |"
+		print "|   *   |   *   |"
+		print "|       |       |"
+	print "+ - - - + - - - +"
+       
+
+ve_hinh()
+
+
