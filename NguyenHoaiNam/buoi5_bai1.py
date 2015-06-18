850ec5cd61a406e79eabf465d19b21ac9a33a947
Nam NH <namptit307@gmail.com>
2015-06-19 00:45:38 +0700

    nop bai tap buoi 6

--- NguyenHoaiNam/buoi5_bai1.py
+++ NguyenHoaiNam/buoi5_bai1.py
@@ -3,16 +3,32 @@
 print """An 1 de tao may ao
 An 2 de xoa may ao
 An 3 de thay doi cau hinh
-An 'q' de thoat chuong trinh"""
+An 'q' de thoat chuong trinh
+An 'p' de hien thong tin may ao"""
+a = []
+tt = []
 while True:
     dau_vao = raw_input("Nhap so: ")
     if dau_vao == '1':
-        print "Qua trinh tao may ao dang duoc thuc hien"
+        ten = raw_input("Nhap ten may: ")
+        ram = raw_input("Nhap ram: ")
+        tt = [ten,ram]
+        a.append(tt)
     elif dau_vao == '2':
-        print "Qua trinh xoa may ao dang duoc thuc hien"
+        print "Qua trinh xoa may"
+        xoa = int(raw_input("Nhap chi so may ao: "))
+        a.pop(xoa)
+        print "May ao con lai:"
+        a
     elif dau_vao == '3':
         print "Qua trinh thay doi cau hinh"
+    elif dau_vao == 'p':
+        print a
     elif dau_vao == 'q':
         exit()
     else:
         print "Ban da nhap sai, hay nhap dung theo yeu cau cua dau bai"
+
+        
+        
+        
